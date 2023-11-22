3afd0ab178ebbff61755bb766c9180ccc05643e2
Dan <dbulman68@gmail.com>
2023-11-22 21:41:36 +0000

    Fixed .Net 8.0 Upgrade

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Danicus2000000.github.io;
+using PortfolioWebsite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
