782ccd543d7f6c5a61d385f96a49b458cab2b325
Daniel Bulman <dbulman68@gmail.com>
2024-04-26 19:01:00 +0100

    Added basic website layout

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <nav class="navbar background">
+        <ul class="nav-list">
+            <div class="logo">
+                <a
+                  href="#Home">
+                  <img src="../icon.ico" alt="Home Icon"/>
+                  </a>
+            </div>
+            <li>
+                <a href="#Home">Home</a>
+            </li>
+            <li>
+                <a href="#Projects">Projects</a>
+            </li>
+            <li>
+                <a href="mailto:testertestington940@gmail.com">Email Me</a>
+            </li>
+            <li>
+                <a href="https://thelanbeforetimes.wordpress.com/">My Blog</a>
+            </li>
+        </ul>
+    </nav>
+
+    <section class="section">
+        <div class="box-main">
+            <div class="firstHalf">
+                <h1 class="text-big">Test section header</h1>
+                <p class="text-small">Test body</p>
+            </div>
+        </div>
+    </section>
+</div>
   );
 }
 
