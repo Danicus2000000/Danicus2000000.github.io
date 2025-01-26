f03385dd846ef69cc948caab76d9dae3966d1054
Daniel Bulman <dbulman68@gmail.com>
2025-01-26 15:42:27 +0000

    Added more skills to the skill carousel and made skill list easier to update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,45 +20,51 @@ export const Skills = () => {
       items: 1
     }
   };
-
+const skillValues = [
+  "C#",
+  "Python",
+  "Azure DevOps",
+  "Java",
+  "C++",
+  "Mobile Development",
+  "Web Development",
+  "Software Development",
+  "YAML",
+  "React",
+  "Android Development",
+  "F#",
+  "SQL",
+  "GraphQL",
+  "TypeScript",
+  "JavaScript",
+  "GitHub Actions"
+];
+const formattedSkillValues = skillValues.map((skillValue, index) => {
   return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>See below a list of my developed skills.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                              <h5>C#</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Python</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Azure DevOps</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Java</h5>
-                            </div>
-                            <div className="item">
-                              <h5>C++</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Mobile Development</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                              <h5>Software Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                </div>
-            </div>
+    <div key={index} className="item">
+      <h5>{skillValue}</h5>
+    </div>
+  );
+});
+return (
+  <section className="skill" id="skills">
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="skill-bx wow zoomIn">
+            <h2>Skills</h2>
+            <p>See below a list of my developed skills.</p>
+            <Carousel
+              responsive={responsive}
+              infinite={true}
+              className="owl-carousel owl-theme skill-slider"
+            >
+              {formattedSkillValues}
+            </Carousel>
+          </div>
         </div>
-    </section>
-  )
+      </div>
+    </div>
+  </section>
+);
 }
