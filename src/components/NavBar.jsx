d39fe036f589448d6e6357bd2c3b03bc3079d46a
Daniel Bulman <dbulman68@gmail.com>
2025-02-04 00:15:03 +0000

    Added expanded mailto link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -42,23 +42,60 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-              <Nav.Link href="mailto:testertestington940@gmail.com" className={'navbar-link'}>Get in touch</Nav.Link>
-              <Nav.Link href="/blog" className={'navbar-link'}>My blog</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="mailto:testertestington940@gmail.com?subject=Website%20-%20get%20in%20touch%20request&body=Hi%20there%2C%0A%0A%7BGive%20some%20request%20context%20here%7D%0A%0AFrom%2C%0A%7BYour%20name%20%2F%20Handle%7D%0A%0ASent%20from%20https%3A%2F%2Fdanicus2000000.github.io%2F"
+                className={"navbar-link"}
+              >
+                Get in touch
+              </Nav.Link>
+              <Nav.Link href="/blog" className={"navbar-link"}>
+                My blog
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://github.com/Danicus2000000"><img src={navIcon2} alt="GitHub" /></a>
+                <a href="https://github.com/Danicus2000000">
+                  <img src={navIcon2} alt="GitHub" />
+                </a>
               </div>
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/daniel-bulman-0b58161b6/"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://www.linkedin.com/in/daniel-bulman-0b58161b6/">
+                  <img src={navIcon1} alt="LinkedIn" />
+                </a>
               </div>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
+  );
 }
