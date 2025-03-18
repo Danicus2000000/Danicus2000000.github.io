03b08af6e4ed9868620ee5b62d17d52e2ad923bd
Daniel Bulman <dbulman68@gmail.com>
2025-03-18 22:05:13 +0000

    added styler

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-multi-carousel/lib/styles.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
